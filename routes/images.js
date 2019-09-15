311f189af676ed1467c769df60e108529f1b6717
unknown <sdi1500089@di.uoa.gr>
2019-09-15 12:00:03 +0300

    Changed a little post and get image

--- routes/images.js
+++ routes/images.js
@@ -9,35 +9,34 @@ const multer = require("multer");
 var upload = multer({ dest: "images/"});
 
 
-router.post('/uploadImage', upload.single('myFile'), (req, res, next) => {
-    const file = req.file
-    if (!file) {
-      const error = new Error('Please upload a file')
-      error.httpStatusCode = 400
-      return next(error)
-    }
-    fs.rename(file.path, './images/' + file.originalname, function(err) {
-        if ( err ) console.log('ERROR: ' + err);
-    });
+// router.post('/uploadImage', upload.single('myFile'), (req, res, next) => {
+//     const file = req.file
+//     if (!file) {
+//       const error = new Error('Please upload a file')
+//       error.httpStatusCode = 400
+//       return next(error)
+//     }
+//     fs.rename(file.path, './images/' + file.originalname, function(err) {
+//         if ( err ) console.log('ERROR: ' + err);
+//     });
+//       res.send(file) 
+// })
 
-    // var nfile = dirname + req.file.filename;
-    // fs.rename(req.file.path, nfile, function(err) {
-    //     if (err) {
-    //     console.log(err);
-    //     res.send(500);
-    //     } else {
-    //         res.json({
-    //             message: 'File uploaded successfully',
-    //             filename: req.file.filename
-    //         });
-    //     }
-    // });
-      res.send(file)
-    
-  })
+router.post('/uploadImage/:newName', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  fs.rename(file.path, './images/' + req.params.newName + '.png', function(err) {
+      if ( err ) console.log('ERROR: ' + err);
+  });
+    res.send(file) 
+})
 
 router.get("/getImage/:name", (req, res) => {
-    res.sendFile(req.params.name, { root: dirname })
+    res.sendFile(req.params.name + '.png', { root: dirname })
 })
 
 
